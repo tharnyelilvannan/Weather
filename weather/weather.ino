0d9d055a466d3853998c9e2b05da2911e3b53065
tharnyelilvannan <tharny2005@outlook.com>
2025-05-14 15:29:24 -0400

    First

---
+++ weather/weather.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+
+#define TFT_CS        10
+#define TFT_RST        8
+#define TFT_DC         9
+#define TFT_MOSI      11
+#define TFT_SCLK      13
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+void setup(void) {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
